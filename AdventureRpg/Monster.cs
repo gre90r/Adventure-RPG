16fe81eb5cefd27220ad9a4ca21309ce2a9dc091
Tor Nygård <Tor@tenrix.com>
2020-07-21 22:41:22 +0200

    editet monster

--- AdventureRpg/Monster.cs
+++ AdventureRpg/Monster.cs
@@ -4,6 +4,10 @@ namespace AdventureRpg
 {
     public class Monster
     {
+
+        // test
+
+
         /*************/
         /* variables */ //Static objects (int string,bool)
         /*************/
